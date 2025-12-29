43caeb2a1d3f2f033ab4a468c1eac3eb1da9f233
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-12-29 18:55:38 +0530

    Add majority element solution using Boyer-Moore
    Implemented Boyer-Moore Voting Algorithm to find the majority element in an array.

--- hashing/0169.java
+++ hashing/0169.java
@@ -1,3 +1,5 @@
+https://leetcode.com/problems/majority-element/
+
 // Boyer-Moore Voting Algorithm
 /*
 1. used to find the majority element among the given elements that have more than N/ 2 occurrences. 
@@ -11,3 +13,17 @@
  6. action is taken when the current array element matches the `candidate = `votes` counter is incremented.
  7. action is taken when the current array element does not match the `candidate` = `votes` counter is decremented.
   */
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count =0;
+        for(int i = 0 ;i<nums.length ;i++){
+            if(count == 0){
+                candidate = nums[i];
+            }
+            count += (nums[i] == candidate) ? +1 :-1;
+        }
+        return candidate;
+    }
+}
