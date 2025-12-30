4edc2a8889f4ab3308a6d3f712a84339ec9cf128
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-12-30 14:07:37 +0530

    Add comments to explain Matrix Chain Multiplication
    Added detailed comments explaining the Matrix Chain Multiplication algorithm, including the primary goal, dimensions derivation, cost calculations, and examples of matrix multiplication.

--- dp/MCM.java
+++ dp/MCM.java
@@ -1,1 +1,27 @@
+MATRIX CHAIN MULTIPLICATION
+  PRIMARY GOAL = To find the most efficient parenthesization for multiplying a 
+                  sequence of matrices to minimize the total number of scalar multiplications.
 
+  TO DERIVE THE DIMENSIONS OF ith MATRIX = The dimensions of the i th
+                                        a[i] =  matrix are `arr[i-1] x arr[i]`.
+                             //previous index and curresnt index = forms the dimension 
+  // arr = 40 30 50 60 20 
+  // a[1] = arr[0] * arr[1] = 40 * 30 
+  //a[2] = 30 * 50 { so b == c property is also maintained }
+  3. the cost in terms of scalar multiplications to multiply a matrix of size m×n by a matrix of size n×p = The number of multiplications is m×n×p
+  4. The time complexity is exponential, O(2^n) = RECURIVE APPROACH
+
+  Matrix to be multiplied :-
+  a*b   c*d
+  1. only mmultipied when b and c equal  {base case can be just return if not same}
+  2. a *d = dimensions of new matrix formed 
+  3. cost = a *b * c [bcz b and c are same so written once] {also called no. of multiplictaion}
+
+
+example 4 marix are give a b c d apply brackets in such a way that cost multiplication ki minimum ho like :-
+  c1 = a*((b*c)*d)
+  c2 = (a*b)*(c*d)
+  c3 = (a*b*c) *d 
+  like ways return minimum among these costs
+  
+ 
