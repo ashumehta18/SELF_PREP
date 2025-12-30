2e22cb0afc485132c68c7a66a41ee237a4db250f
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-12-30 10:59:09 +0530

    Implement comments for DFS algorithm explanation
    Added comments explaining depth-first search algorithm.

---
+++ tree/DFS.java
@@ -0,0 +1,11 @@
+// depth first search
+
+1. It starts from a source vertex and 
+       explores each path as completely as possible
+                              before backtracking to explore other paths.
+
+ 2.  TO avoid revisiting of array = A 'visited' array or set is used to keep track of vertices 
+                                     that have already been visited.
+
+  3. Yes, the traversal order can change depending on the order in which adjacent vertices are selected to be visited
+    === single graph have more than one valid DFS traversal sequence
