426ceaf7a88702cb300fe5b3f3c160fc9476d3ef
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-12-31 09:04:41 +0530

    Document binary heap and heap sort algorithm
    Added comments explaining the binary heap and heap sort algorithm.

--- heap/heapSort.java
+++ heap/heapSort.java
@@ -1,1 +1,8 @@
+ Binary Heap  = data structure
+   optimized version of selection sort.
+   The algorithm repeatedly finds the maximum (or minimum) element and swaps it with the last (or first) element.
+   O(log N) = time complexity
 
+   steps :-
+   1. build heap using heapify either max or min heap 
+   2.
