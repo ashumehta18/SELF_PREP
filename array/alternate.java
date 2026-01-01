39acf8e74e68d8430339b9a1a4db18efe9379814
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-01 11:53:51 +0530

    Add method to retrieve alternate elements from array
    Implement method to get alternate elements from an array.

---
+++ array/alternate.java
@@ -0,0 +1,14 @@
+https://www.geeksforgeeks.org/problems/print-alternate-elements-of-an-array/1
+class Solution {
+    // function to print alternate elements of an array
+    public ArrayList<Integer> getAlternates(int arr[]) {
+        // Code Here
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i<arr.length ; i++){
+            if(i%2 == 0){
+                list.add(arr[i]);
+            }
+        }
+        return list;
+    }
+}
