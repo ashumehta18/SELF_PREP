d74aebb2df586e3db265ab6b0638455b8079496f
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-04 08:58:21 +0530

    Add problem description for min_cost.java
    Added a description for the minimum cost problem.

--- greedy/min_cost.java
+++ greedy/min_cost.java
@@ -1,2 +1,2 @@
 //minimum cost to make array size 1 by removing the larger of values
-
+Given an array of n integers. We need to reduce size of array to one. We are allowed to select a pair of integers and remove the larger one of these two. This decreases the array size by 1. Cost of this operation is equal to value of smallest one. Find out minimum sum of costs of operations needed to convert the array into a single element.
