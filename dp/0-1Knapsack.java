f496f4062ecfcc175f3a8752affd38d3208e732d
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-07 00:07:56 +0530

    Add knapsack function using dynamic programming
    Implement dynamic programming solution for 0-1 Knapsack problem.

--- dp/0-1Knapsack.java
+++ dp/0-1Knapsack.java
@@ -1,1 +1,22 @@
 
+class Solution {
+    public int knapsack(int W, int val[], int wt[]) {
+
+        int n = wt.length;              // number of items
+        int[][] dp = new int[n+1][W+1]; // DP table
+
+        for(int i = 1; i <= n; i++){        // items
+            for(int j = 1; j <= W; j++){    // capacity
+                if(wt[i-1] <= j){           // item fits
+                    dp[i][j] = Math.max(
+                        val[i-1] + dp[i-1][j - wt[i-1]], // include
+                        dp[i-1][j]                       // exclude
+                    );
+                } else {
+                    dp[i][j] = dp[i-1][j]; // cannot include Ignore curr item Carry forward previous result
+                }
+            }
+        }
+        return dp[n][W];
+    }
+}
