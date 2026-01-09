5edcab0680959d1c8837d4e6a2d6e6a29b467314
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-09 20:18:33 +0530

    Enhance comments in getAlternates method
    Added comments to clarify the purpose of the code.

--- array/alternate.java
+++ array/alternate.java
@@ -3,9 +3,9 @@ class Solution {
     // function to print alternate elements of an array
     public ArrayList<Integer> getAlternates(int arr[]) {
         // Code Here
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>(); //create 
         for(int i = 0 ; i<arr.length ; i++){
-            if(i%2 == 0){
+            if(i%2 == 0){ //check for positive index and add it in list 
                 list.add(arr[i]);
             }
         }
