7ae6859b981b39fc26acee27f8fdfb074bc4b543
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-18 18:44:40 +0530

    Add BFS traversal method in Solution class
    Implement BFS traversal for a graph in Java.

--- tree/BFS.java
+++ tree/BFS.java
@@ -1,1 +1,30 @@
 
+https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+
+class Solution {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        int v = adj.size();
+        boolean vis[]  = new boolean[v];
+        
+        int source = 0;
+        Queue<Integer> q = new LinkedList<>();
+        vis[source] = true;
+        q.add(source);
+        
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            res.add(curr);
+            
+            for(int x : adj.get(curr)){
+            if(!vis[x]){
+                vis[x] = true;
+                q.add(x);
+            }
+            }
+            
+        }
+        return res;
+    }
+}
