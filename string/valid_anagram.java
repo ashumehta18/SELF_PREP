47247f3a60004d5537a8626a740d4fc7724cf920
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-18 21:30:10 +0530

    Add valid_anagram.java with anagram checking logic
    Implement method to check if two strings are anagrams.

---
+++ string/valid_anagram.java
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/valid-anagram/
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        char[] a1 = s.toCharArray();
+        char[] a2 = t.toCharArray();
+
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+
+        return Arrays.equals(a1,a2);
+
+
+    }
+}
