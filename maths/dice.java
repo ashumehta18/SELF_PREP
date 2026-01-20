2c488cc1b178e30ad671576f7350d505ac66c9f4
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-20 10:43:53 +0530

    Add function to find opposite face of a dice

---
+++ maths/dice.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+https://www.geeksforgeeks.org/problems/the-dice-problem2316/1
+class Solution {
+    static int oppositeFaceOfDice(int n) {
+        // code here
+        return 7-n;
+    }
+}
