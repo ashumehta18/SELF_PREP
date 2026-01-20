9b0d548cb126be796a0d3cb744d72808710b9ec7
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-20 10:46:35 +0530

    Add reference link for AP series problem
    Added a reference link for the AP series problem.

---
+++ AP_term.java
@@ -0,0 +1,9 @@
+https://www.geeksforgeeks.org/problems/series-ap5310/1
+class Solution {
+    public static int nthTermOfAP(int a1, int a2, int n) {
+        // code here
+        int d = a2-a1;
+        int ans = a1+(n-1)*d;
+        return ans;
+    }
+}
