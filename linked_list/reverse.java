c3974c5c907da1ab60dd2d01b798df9b32f6d7da
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-20 23:57:33 +0530

    Add link to GeeksforGeeks for reversing a linked list

---
+++ linked_list/reverse.java
@@ -0,0 +1,29 @@
+https://www.geeksforgeeks.org/problems/reverse-a-linked-list/1
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+        while(curr != null){
+           next = curr.next;
+           curr.next = prev;
+           prev = curr;
+          curr = next;
+           //curr = curr.next;
+        }
+        return prev;
+    }
+}
