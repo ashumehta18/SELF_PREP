a4b1ed0360f5cea327d51f8ff0e91fb3795265ea
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-21 13:01:55 +0530

    Add method to find missing number in array
    Implement missing number calculation in array.

--- array/missing_arr.java
+++ array/missing_arr.java
@@ -1,1 +1,14 @@
-https://www.geeksforgeeks.org/problems/missing-number-in-array1416/1?page=1&sortBy=submissions
+https://www.geeksforgeeks.org/problems/missing-number-in-array1416/1
+class Solution {
+    int missingNum(int arr[]) {
+        // code here
+       long s1 = 0;
+       
+       long n = arr.length +1 ;
+       long s2 = n*(n+1)/2;
+       for(int i = 0 ;i<arr.length;i++){
+           s1 +=arr[i];
+       }
+       return  (int)(s2-s1);
+    }
+}
