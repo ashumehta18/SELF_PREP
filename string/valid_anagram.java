0c2f1d720d5449d49ace0b4c2e358d32ac7aaa91
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-21 05:30:03 +0530

    Add method to compare two strings for equality
    Implemented a method to check if two strings are the same.

--- string/valid_anagram.java
+++ string/valid_anagram.java
@@ -15,3 +15,29 @@ class Solution {
 
     }
 }
+
+
+// strings are same or not ?
+https://www.geeksforgeeks.org/problems/check-if-two-strings-are-same-or-not--155406/1
+// User function Template for Java
+class StringUtils {
+    // Function to check if two strings are the same
+    public static boolean areStringsSame(String s1, String s2) {
+        // code here
+        // if(s1.length() == null || s2.length() == null){
+        //     return false;
+        // }
+        // if(s1.length() == null && s2.length() == null){
+        //     return false;
+        //}
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        for(int i = 0 ; i<s1.length() ;i++){
+            if(s1.charAt(i) != s2.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
