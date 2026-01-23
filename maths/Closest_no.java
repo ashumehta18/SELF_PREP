7ab5890262a86aa7cc4e386b3605e51ed6ed7d94
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-01-23 13:24:29 +0530

    Simplify closestNumber logic and improve readability
    Refactor closestNumber method for simplicity and clarity.

--- maths/Closest_no.java
+++ maths/Closest_no.java
@@ -2,28 +2,12 @@ https://www.geeksforgeeks.org/problems/closest-number5728/1
 
 class Solution {
     static int closestNumber(int n, int m) {
-        int q = n / m;
-
-        int n1 = m * q;
-        int n2 = m * (q + 1);
-        int n3 = m * (q - 1); 
-
-        int d1 = Math.abs(n1 - n);
-        int d2 = Math.abs(n2 - n);
-        int d3 = Math.abs(n3 - n);
-
-        int res = n1;
-        int minDist = d1;
-
-        if (d2 < minDist || (d2 == minDist && Math.abs(n2) > Math.abs(res))) {
-            res = n2;
-            minDist = d2;
-        }
-
-        if (d3 < minDist || (d3 == minDist && Math.abs(n3) > Math.abs(res))) {
-            res = n3;
-        }
-
-        return res;
+        int q = n/m; //recive quotient
+        
+        int n1 = m*q; //first no.
+        
+        int n2 = n*m>0 ? m*(q+1) : m*(q-1); // depends upon sign -ve if handles easily 
+        
+         return (Math.abs(n-n1)<Math.abs(n-n2)) ? n1 :n2; // which difference is lower
     }
 }
