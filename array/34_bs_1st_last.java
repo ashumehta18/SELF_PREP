d0507fc6f417c6c3a834aea13dcff4c531fd3f73
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2026-02-09 17:20:35 +0530

    Implement binary search for first and last positions
    Refactor to use two functions for finding first and last positions of a target in a sorted array.

--- array/34_bs_1st_last.java
+++ array/34_bs_1st_last.java
@@ -1,1 +1,48 @@
 https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+// 1. brute = by linear search 
+//2. the way performed using 2 different function 
+//3. but it can be performed using one single function
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int res[] = new int[2];
+        res[0] = f(nums,target);
+        res[1] = l(nums,target);
+        return res;
+    }
+     public int f(int[] nums, int target) {
+        int left = 0 ;
+        int right = nums.length -1;
+        int ans = -1;
+        while(left<=right){
+            int mid = left +(right - left)/2;
+            if(nums[mid] == target){
+                ans =  mid;
+                right = mid -1;
+            }
+            else if(nums[mid]>target){
+                right = mid -1;
+            } else{
+                left = mid+1;
+            }
+        }
+        return ans;
+     }
+     public int l(int[] nums, int target) {
+        int left = 0 ;
+        int right = nums.length -1;
+        int ans = -1;
+        while(left<=right){
+            int mid = left +(right - left)/2;
+            if(nums[mid] == target){
+                ans =  mid;
+                left = mid +1;
+            }
+            else if(nums[mid]>target){
+                right = mid -1;
+            } else{
+                left = mid+1;
+            }
+        }
+        return ans;
+     }
+}
